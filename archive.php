70532df251da5500a248807d9f255cf3261a8600
younes-dro <dro66_8@hotmail.fr>
2018-11-20 18:53:32 +0100

    Add container fluid in page Archive

--- archive.php
+++ archive.php
@@ -6,52 +6,52 @@
  *
  * @package dro_caterer
  */
-
 get_header();
 ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-                    
-		<?php if ( have_posts() ) : ?>
+                    <?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<?php
+                        <header class="page-header">
+                            <?php
+                            echo single_cat_title('<h1 class="page-title">');
+                            echo get_the_type_menu_price($wp_query->queried_object->term_id);
+                            the_archive_description('<div class="archive-description text-muted">', '</div>');
+                            ?>
+                        </header><!-- .page-header -->
 
-                                echo  single_cat_title('<h1 class="page-title">');
-                                echo get_the_type_menu_price($wp_query->queried_object->term_id);
-				the_archive_description( '<div class="archive-description text-muted">', '</div>' );
-                                
-				?>
-			</header><!-- .page-header -->
-                        
                         <div class="row justify-content-md-center">
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+                            <?php
+                            /* Start the Loop */
+                            while (have_posts()) :
+                                the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/post/content', 'custom-tax' );
+                                /*
+                                 * Include the Post-Type-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                                 */
+                                get_template_part('template-parts/post/content', 'custom-tax');
 
-			endwhile;
+                            endwhile;
 
-			the_posts_navigation();
+                            the_posts_navigation();
 
-		else :
+                        else :
 
-			get_template_part( 'template-parts/content', 'none' );
+                            get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
+                        endif;
+                        ?>
                     </div><!-- ./row -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+                </main><!-- #main -->
+            </div><!-- #primary -->
+        </div><!-- ./col-12-->
+    </div><!-- .row -->
+</div><!-- ./ container-fluid -->
 <?php
-//get_sidebar();
 get_footer();
